19cdd5e6de4e916cc033a4298e264def920a2bf9
Shugyla0077 <Shugylaa2085@gmail.com>
2025-10-08 13:51:42 +0500

    first lab

---
+++ lab.4/10.py
@@ -0,0 +1,10 @@
+
+height = 5
+base1 = 5
+base2 = 6
+
+# Формула площади трапеции: (1/2) * (base1 + base2) * height
+area = 0.5 * (base1 + base2) * height
+
+# Вывод результата
+print("Площадь трапеции:", area)
---
+++ lab.4/13.py
@@ -0,0 +1,10 @@
+import json
+
+
+json_string = '{"name": "John", "age": 30, "city": "New York"}'
+
+
+data = json.loads(json_string)
+
+
+print(data)
